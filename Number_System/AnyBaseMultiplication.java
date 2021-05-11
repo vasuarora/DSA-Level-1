43aea6194cd21bad54183130ca87a9d07516bf79
Vasu Arora <vasuarora2112@gmail.com>
2021-05-12 00:23:58 +0530

    codes

---
+++ Number_System/AnyBaseMultiplication.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class AnyBaseMultiplication {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int b = scn.nextInt();
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+    
+        int d = getProduct(b, n1, n2);
+        System.out.println(d);
+     }
+    
+     public static int getProduct(int b, int n1, int n2){
+         int p=1;
+         int s=0;
+         while(n2!=0){
+             int d2=n2%10;
+             n2=n2/10;
+             int t=prodSingleDigit(b,n1,d2);
+             s=add(b,s,t*p);
+             p=p*10;
+         }
+         return s;
+     }
+     
+     public static int prodSingleDigit(int b,int n1,int d2){
+         int ans=0;
+         int pow=1;
+         int c=0;
+         while(n1>0 || c>0){
+             int d1=n1%10;
+             n1=n1/10;
+             
+             int mult=(d2*d1)+c;
+             int s=mult%b;
+             c=mult/b;
+             ans=ans+s*pow;
+             pow=pow*10;
+         }
+         return ans;
+     }
+     
+     public static int add(int b,int n1,int n2){
+         int c=0;
+         int ans=0;
+         int pow=1;
+           
+         while(n1!=0 || n2!=0 || c!=0){
+             int d1=n1%10;
+             int d2=n2%10;
+             n1=n1/10;
+             n2=n2/10;
+               
+             int s=d1+d2+c;
+             int sum=s%b;
+             c=s/b;
+               
+             ans=ans+sum*pow;
+             pow=pow*10;
+           }
+         return ans;
+     }
+}
---
+++ Number_System/AnyBaseSubtraction.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class AnyBaseSubtraction {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int b = scn.nextInt();
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+    
+        int d = getDifference(b, n1, n2);
+        System.out.println(d);
+     }
+    
+     public static int getDifference(int b, int n1, int n2){
+         int ans=0;
+         int pow=1;
+         int d=0;
+         
+         while(n2!=0){
+             int d1=n1%10;
+             int d2=n2%10;
+             
+             n1=n1/10;
+             n2=n2/10;
+             
+             d2=d2+d;
+             int diff=0;
+             
+             if(d2>=d1){
+                 diff=d2-d1;
+                 d=0;
+             }
+             else{
+                 diff=d2+b-d1;
+                 d=-1;
+             }
+             
+             ans=ans+diff*pow;
+             pow=pow*10;
+         }
+         return ans;
+     }
+}
