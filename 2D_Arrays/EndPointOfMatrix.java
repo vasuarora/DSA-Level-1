64bd6a7b023474a611119461ff522ad69116cc4c
Vasu Arora <vasuarora2112@gmail.com>
2021-05-18 00:26:28 +0530

    codes

---
+++ 2D_Arrays/EndPointOfMatrix.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class EndPointOfMatrix {
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int r=scn.nextInt();
+        int c=scn.nextInt();
+        int[][] a=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                a[i][j]=scn.nextInt();
+            }
+        }
+        
+        int dir=0;
+        int i=0;
+        int j=0;
+        while(true){
+            dir=(a[i][j]+dir)%4;
+            
+            if(dir==0){
+                j++;
+            }
+            
+            else if(dir==1){
+                i++;
+            }
+            
+            else if(dir==2){
+                j--;
+            }
+            
+            else if(dir==3){
+                i--;
+            }
+            
+            if(i<0){
+                i++;
+                break;
+            }
+            
+            else if(j<0){
+                j++;
+                break;
+            }
+            
+            else if(i==a.length){
+                i--;
+                break;
+            }
+            
+            else if(j==a[0].length){
+                j--;
+                break;
+            }
+        }
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
---
+++ 2D_Arrays/RotateBy90.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class RotateBy90 {
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[][] arr=new int[n][n];
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=scn.nextInt();
+            }
+        }
+    
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr[0].length;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        
+        int li=0;
+        int ri=arr[0].length-1;
+        while(li<ri){
+            for(int i=0;i<n;i++){
+                int temp=arr[i][li];
+                arr[i][li]=arr[i][ri];
+                arr[i][ri]=temp;
+            }
+            li++;
+            ri--;
+        }
+        display(arr);
+    }
+
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
