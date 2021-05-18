5ca1572dfcda4c3cf6d0a8cb92bb8a1169c9564b
Vasu Arora <vasuarora2112@gmail.com>
2021-05-19 00:26:46 +0530

    codes

--- 2D_Arrays/Matrix_Multiplication.java
+++ 2D_Arrays/Matrix_Multiplication.java
@@ -5,7 +5,8 @@ public class Matrix_Multiplication {
         int r1=scn.nextInt();
         int c1=scn.nextInt();
         int[][] a1=new int[r1][c1];
-        for(int i=0;i<a1.length;i++){
+        
+        for(int i=0;i<a1.length;i++){                            //Matrix 1 input
             for(int j=0;j<a1[0].length;j++){
                 a1[i][j]=scn.nextInt();
             }
@@ -14,18 +15,18 @@ public class Matrix_Multiplication {
         int r2=scn.nextInt();
         int c2=scn.nextInt();
         int[][] a2=new int[r2][c2];
-        for(int i=0;i<a2.length;i++){
+        for(int i=0;i<a2.length;i++){                             //Matrix 2 input
             for(int j=0;j<a2[0].length;j++){
                 a2[i][j]=scn.nextInt();
             }
         }
         
-        if(c1!=r2){
+        if(c1!=r2){                                               //To check whether multiplication is possible or not
             System.out.println("Invalid input");
             return;
         }
         
-        int[][] a3=new int[r1][c2];
+        int[][] a3=new int[r1][c2];                                             
         for(int i=0;i<r1;i++){
             for(int j=0;j<c2;j++){
                 for(int k=0;k<c1;k++){
@@ -34,7 +35,7 @@ public class Matrix_Multiplication {
             }
         }
         
-        for(int i=0;i<a3.length;i++){
+        for(int i=0;i<a3.length;i++){                               //Displaying Matrix 3
             for(int j=0;j<a3[0].length;j++){
                 System.out.print(a3[i][j]+" ");
             }
