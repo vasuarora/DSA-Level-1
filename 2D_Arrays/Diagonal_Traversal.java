f8d81e9bbd06e7f30eb74adeab1a5125097bb804
Vasu Arora <vasuarora2112@gmail.com>
2021-05-20 00:49:27 +0530

    codes

---
+++ 2D_Arrays/Diagonal_Traversal.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Diagonal_Traversal {
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[][] a=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]=scn.nextInt();
+            }
+        }
+        
+        for(int g=0;g<a.length;g++){
+            for(int i=0,j=g;j<a[0].length;i++,j++){
+                System.out.println(a[i][j]);
+            }
+        }
+    }
+}
