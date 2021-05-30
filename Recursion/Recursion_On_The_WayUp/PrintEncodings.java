5ae98c47199b4290deca5f26be7ce48ee773902d
Vasu Arora <vasuarora2112@gmail.com>
2021-05-31 00:42:17 +0530

    codes

---
+++ Recursion/Recursion_On_The_WayUp/PrintEncodings.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class PrintEncodings {
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        String str=scn.next();
+        printEncodings(str,"");
+    }
+
+    public static void printEncodings(String str,String asf) {
+        if(str.length()==0){
+            System.out.println(asf);                      //printing the encoding 
+            return;
+        }
+        
+        char ch=str.charAt(0);               
+        if(ch=='0'){                             //checking that if the 1st character of string is 0
+            return;
+        }
+        
+        String ros=str.substring(1);                    //remaining string
+        char enc=(char)(ch-'0'+'a'-1);                  //encoding for corresponding single digit number
+        
+        printEncodings(ros,asf+enc);
+        
+        if(str.length()>=2){                          //to find encoding for corresponding double digit number
+            int tp=str.charAt(0)-'0';
+            int up=str.charAt(1)-'0';
+            int val=tp*10+up;                         //creating that double digit number from string
+            
+            if(val>=10 && val<=26){                    //to check if the double digit number lies between a valid range of characters
+                String ros2=str.substring(2);
+                char enc2=(char)(val-1+'a');
+                printEncodings(ros2,asf+enc2);
+            }
+        }
+    }
+}
