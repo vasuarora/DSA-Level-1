276eac2239f62c2082c49213e8bb2693e396a228
Vasu Arora <vasuarora2112@gmail.com>
2021-06-06 00:11:59 +0530

    codes

--- DynamicProgrammingAndGreedy/ClimbStairs.java
+++ DynamicProgrammingAndGreedy/ClimbStairs.java
@@ -3,20 +3,20 @@ public class ClimbStairs {
     public static void main(String[] args) throws Exception {
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
-        int[] dp=new int[n+1];
+        int[] dp=new int[n+1];                
         dp[0]=1;
         
         for(int i=1;i<dp.length;i++){
             if(i==1){                          
-                dp[i]=dp[i-1];                         //no. of paths to climb 1 stair
+                dp[i]=dp[i-1];                         //storing the count of paths to climb 1 stair in the dp array
             }
             
             else if(i==2){                     
-                dp[i]=dp[i-1]+dp[i-2];                //no. of paths to climb 2 stairs
+                dp[i]=dp[i-1]+dp[i-2];                //storing the count of paths to climb 2 stairs in the dp array
             }
             
             else{
-                dp[i]=dp[i-1]+dp[i-2]+dp[i-3];           //no. of paths to climb i stairs
+                dp[i]=dp[i-1]+dp[i-2]+dp[i-3];           //storing the count of paths to climb i stairs in the dp array
             }
         }
         
--- DynamicProgrammingAndGreedy/FibonacciSeries.java
+++ DynamicProgrammingAndGreedy/FibonacciSeries.java
@@ -5,10 +5,10 @@ public class FibonacciSeries {
         int n=scn.nextInt();
         int[] dp=new int[n+1];
         
-        dp[0]=0;                             //fibonacci of 0
-        dp[1]=1;                             //fibonacci of 1
+        dp[0]=0;                             //storing fibonacci of 0 in the dp array
+        dp[1]=1;                             //storing fibonacci of 1 in the dp array
         for(int i=2;i<dp.length;i++){
-            dp[i]=dp[i-1]+dp[i-2];                  //storing the fibonacci of ith number in dp array           
+            dp[i]=dp[i-1]+dp[i-2];                  //storing the fibonacci of ith number in the dp array           
         }
         
         System.out.println(dp[n]);               //printing the fibonacci of the given number
