554cffb0472dbb23f7c9ed9e4c539ccd2fa8b2ea
Vasu Arora <vasuarora2112@gmail.com>
2021-06-09 00:30:51 +0530

    codes

---
+++ DynamicProgrammingAndGreedy/CoinChangePermutations.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class CoinChangePermutations {
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+        
+        int amt=scn.nextInt();
+        
+        int ans=permutation(arr,n,amt);
+        System.out.println(ans);
+    }
+    
+    public static int permutation(int[] arr,int n,int amt){
+        int[] dp=new int[amt+1];
+        dp[0]=1;
+        
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<arr.length;j++){
+                int rem_amt=i-arr[j];
+                if(rem_amt>=0){
+                    dp[i]+=dp[rem_amt];
+                }
+            }
+        }
+        
+        return dp[amt];
+        
+    }
+}
