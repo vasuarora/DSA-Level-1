4c85491603f48b6d72794a5a44a8804f8f793c1b
Vasu Arora <vasuarora2112@gmail.com>
2021-06-10 00:45:57 +0530

    codes

--- DynamicProgrammingAndGreedy/CountBinaryStrings.java
+++ DynamicProgrammingAndGreedy/CountBinaryStrings.java
@@ -7,8 +7,8 @@ public class CountBinaryStrings {
         int oz=1;
         
         for(int i=2;i<=n;i++){
-            int nz=oo;                  //numbers ending with zero with no consecutive 0's
-            int no=oo+oz;               //numbers ending with one with no consecutive 0's
+            int nz=oo;                  //numbers ending at zero with no consecutive 0's
+            int no=oo+oz;               //numbers ending at one with no consecutive 0's
             
             oz=nz;
             oo=no;
