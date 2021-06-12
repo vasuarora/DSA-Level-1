a173d121e579227d7558c0783ffebe3d7e3bfd2b
Vasu Arora <vasuarora2112@gmail.com>
2021-06-13 00:28:57 +0530

    codes

---
+++ DynamicProgrammingAndGreedy/TilingWith2X1Tiles.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class TilingWith2X1Tiles {
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] dp=new int[n+1];
+        
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        System.out.println(dp[n]);
+    }
+}
