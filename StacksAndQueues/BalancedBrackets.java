69bfd09b51ab5477a36cfefafb2112aafe81e152
Vasu Arora <vasuarora2112@gmail.com>
2021-06-17 00:44:04 +0530

    codes

--- StacksAndQueues/BalancedBrackets.java
+++ StacksAndQueues/BalancedBrackets.java
@@ -8,12 +8,12 @@ public class BalancedBrackets {
         for(int i=0;i<str.length();i++){
             char c=str.charAt(i);
             
-            if(c=='(' || c=='[' || c=='{'){
+            if(c=='(' || c=='[' || c=='{'){             //pushing the opening brackets in stack
                 st.push(c);
             }
             
             else if(c==')'){
-                boolean a=bracket(st,'(');
+                boolean a=bracket(st,'(');             //checking if the closing bracket matches with its opening bracket pair
                 if(a==false){
                     System.out.println(a);
                     return;
@@ -21,7 +21,7 @@ public class BalancedBrackets {
             }
             
             else if(c==']'){
-                boolean a=bracket(st,'[');
+                boolean a=bracket(st,'[');            //checking if the closing bracket matches with its opening bracket pair
                 if(a==false){
                     System.out.println(a);
                     return;
@@ -29,14 +29,14 @@ public class BalancedBrackets {
             }
             
             else if(c=='}'){
-                boolean a=bracket(st,'{');
+                boolean a=bracket(st,'{');              //checking if the closing bracket matches with its opening bracket pair
                 if(a==false){
                     System.out.println(a);
                     return;
                 }
             }
         }
-        if(st.size()==0){
+        if(st.size()==0){                               //if the size of stack is zero,it means there is no extra opening bracket
             System.out.println("true");
         }
         else{
@@ -45,14 +45,14 @@ public class BalancedBrackets {
     }
     
     public static boolean bracket(Stack<Character> st,char c){
-        if(st.size()==0){
+        if(st.size()==0){                             //if the size of stack is zero it means there is an extra closing bracket
             return false;
         }
         else if(st.peek()!=c){
             return false;
         }
         else{
-            st.pop();
+            st.pop();                                 //popping out the bracket from stack
             return true;
         }
     }
