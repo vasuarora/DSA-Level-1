7279b34e7cd6987e682acc57ede0595fafe0d51d
Vasu Arora <vasuarora2112@gmail.com>
2021-06-25 02:04:23 +0530

    codes

---
+++ StacksAndQueues/SmallestNumberFollowingPattern.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class SmallestNumberFollowingPattern {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+    
+        int val=1;
+        Stack<Integer> st=new Stack<>();
+        
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            
+            if(ch=='d'){
+                st.push(val);
+                val++;
+            }
+            
+            else{
+                st.push(val);
+                val++;
+                
+                while(st.size()>0){
+                    System.out.print(st.pop());
+                }
+            }
+            
+        }
+        
+        st.push(val);
+            
+        while(st.size()>0){
+            System.out.print(st.pop());
+        }
+     }
+}
