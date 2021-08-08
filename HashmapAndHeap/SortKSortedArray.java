421dbc7b3cbbf2c22ce0d28fdfa1e8b0baae07ca
Vasu Arora <vasuarora2112@gmail.com>
2021-08-09 03:36:07 +0530

    Heaps

---
+++ HashmapAndHeap/SortKSortedArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class SortKSortedArray {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+  
+        for (int i = 0; i < n; i++) {
+           arr[i] = Integer.parseInt(br.readLine());
+        }
+  
+        int k = Integer.parseInt(br.readLine());
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        
+        for(int i=0;i<arr.length;i++){
+            if(i<=k){
+                pq.add(arr[i]);
+            }
+            
+            else{
+                System.out.println(pq.remove());
+                pq.add(arr[i]);
+            }
+        }
+        
+        while(pq.size()>0){
+            System.out.println(pq.remove());
+        }
+     }
+}
