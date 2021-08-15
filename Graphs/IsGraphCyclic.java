a35e603f7f979dbee85a1134ea0edd498eb0c8d6
Vasu Arora <vasuarora2112@gmail.com>
2021-08-16 00:57:30 +0530

    Graphs

---
+++ Graphs/IsGraphCyclic.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+
+public class IsGraphCyclic {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+  
+        Edge(int src, int nbr, int wt) {
+           this.src = src;
+           this.nbr = nbr;
+           this.wt = wt;
+        }
+     }
+  
+     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+           graph[i] = new ArrayList<>();
+        }
+  
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+           String[] parts = br.readLine().split(" ");
+           int v1 = Integer.parseInt(parts[0]);
+           int v2 = Integer.parseInt(parts[1]);
+           int wt = Integer.parseInt(parts[2]);
+           graph[v1].add(new Edge(v1, v2, wt));
+           graph[v2].add(new Edge(v2, v1, wt));
+        }
+        
+        boolean[] vis=new boolean[graph.length];
+        
+        for(int i=0;i<graph.length;i++){
+            if(vis[i]==false){
+                boolean ans=isCyclic(graph,i,vis);
+                
+                if(ans==true){
+                    System.out.println("true");
+                    return;
+                }
+            }
+            
+        }
+        
+        System.out.println("false");
+      
+     }
+     
+     public static boolean isCyclic(ArrayList<Edge>[] graph,int src,boolean[] vis){
+         
+         Queue<Integer> q=new ArrayDeque<>();
+         
+         q.add(src);
+         
+         while(q.size()>0){
+             int rem=q.remove();
+             
+             if(vis[rem]==true){
+                 return true;
+             }
+             
+             vis[rem]=true;
+             
+             for(Edge ne:graph[rem]){
+                 int nbr=ne.nbr;
+                 
+                 if(vis[nbr]==false){
+                     q.add(nbr);
+                 }
+             }
+         }
+         
+         return false;
+         
+     }
+}
