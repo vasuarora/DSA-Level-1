12031698eac3a8fa3cadfef56c24a81f19713a58
Vasu Arora <vasuarora2112@gmail.com>
2021-08-18 00:30:39 +0530

    Prims Algo

---
+++ Graphs/MinWireToConnectPc.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+
+public class MinWireToConnectPc {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+  
+        Edge(int src, int nbr, int wt) {
+           this.src = src;
+           this.nbr = nbr;
+           this.wt = wt;
+        }
+     }
+     
+     public static class pair implements Comparable<pair>{
+         int vtx;
+         int acv;
+         int wt;
+         
+         pair(){
+             
+         }
+         
+         pair(int vtx,int acv,int wt){
+             this.vtx=vtx;
+             this.acv=acv;
+             this.wt=wt;
+         }
+         
+         public int compareTo(pair o){
+             return this.wt-o.wt;
+         }
+     }
+     
+     public static void minWire(ArrayList<Edge>[] graph){
+         PriorityQueue<pair> pq=new PriorityQueue<>();
+         pq.add(new pair(0,-1,0));
+         
+         boolean[] vis=new boolean[graph.length];
+         
+         while(pq.size()>0){
+             pair rem=pq.remove();
+             
+             if(vis[rem.vtx]==true){
+                 continue;
+             }
+             
+             vis[rem.vtx]=true;
+             
+             if(rem.acv!=-1){
+                 System.out.println("["+rem.vtx+"-"+rem.acv+"@"+rem.wt+"]");
+             }
+             
+             for(Edge ne:graph[rem.vtx]){
+                 int nbr=ne.nbr;
+                 
+                 if(vis[nbr]==false){
+                     pq.add(new pair(nbr,rem.vtx,ne.wt));
+                 }
+             }
+         }
+     }
+  
+     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+           graph[i] = new ArrayList<>();
+        }
+  
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+           String[] parts = br.readLine().split(" ");
+           int v1 = Integer.parseInt(parts[0]);
+           int v2 = Integer.parseInt(parts[1]);
+           int wt = Integer.parseInt(parts[2]);
+           graph[v1].add(new Edge(v1, v2, wt));
+           graph[v2].add(new Edge(v2, v1, wt));
+        }
+  
+        minWire(graph);
+     }
+}
